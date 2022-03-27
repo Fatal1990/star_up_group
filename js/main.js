586831feac9a69eb825dc77ff42b221f78d879d6
Fatal1990 <96417741+Fatal1990@users.noreply.github.com>
2022-03-27 14:13:50 +0300

    start

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-   const active = document.getElementById ('burger_active');
-   const nav = document.getElementById ('nav_active')
-   const lock = document.getElementById ('lock')
-   console.log(active); 
-   active.onclick = function () {
-    active.classList.toggle ('header__burger-active')
-    nav.classList.toggle ('header__nav-wrp-active')
-    lock.classList.toggle ('body-lock')
+    const nav = document.getElementById('nav');
+    const body = document.querySelector('body');
+    const lock = nav.querySelector('.header__nav-wrp');
+    const burger = nav.querySelector('.header__burger');
+    burger.onclick = function () {
+        burger.classList.toggle('header__burger-active');
+        lock.classList.toggle('header__nav-wrp-active');
+        body.classList.toggle('body-lock')
     }
 }  
